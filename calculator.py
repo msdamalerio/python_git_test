bee87d97feef0972aa49fea50d9dcf21cdf1778d
msdamalerio <msdamalerio@up.edu.ph>
2022-02-22 11:55:43 +0800

    add multiply function and description

--- calculator.py
+++ calculator.py
@@ -6,3 +6,7 @@ def add(num1, num2):
 # This function is for subtraction
 def subtract(num1, num2):
     return num1 - num2
+
+# This function is for multiplication
+def multiply(num1, num2):
+    return num1 * num2
