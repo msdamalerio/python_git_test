3c7e71df25c4dd32d4be24fbfbb7a3e02cc07e12
msdamalerio <msdamalerio@up.edu.ph>
2022-02-22 12:29:50 +0800

    add feature 2

--- calculator.py
+++ calculator.py
@@ -10,3 +10,7 @@ def subtract(num1, num2):
 # This function is for multiplication
 def multiply(num1, num2):
     return num1 * num2
+
+# feature 2: (branch 2)
+def feature2():
+    print("Added feature 2!")
